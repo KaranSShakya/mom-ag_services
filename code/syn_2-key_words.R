9a12b5a13c36a68d7c85fcf6acf7e19a58aced67
KaranSShakya <karan.fingo@gmail.com>
2020-05-31 15:32:38 -0700

    Synthesis analysis - task 2 complete

---
+++ code/syn_2-key_words.R
@@ -0,0 +1,105 @@
+library(tidyverse)
+library(readxl)
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+library(SnowballC)
+
+data0 <- read_excel("data/synthesis_HKH-copy.xlsx") %>% 
+  select(-35:-39)
+
+#Author Keyword -------
+data.a <- data0 %>% 
+  select(4, 18)
+
+sum(is.na(data.a$Auth_keywords)) #123 rows are NA
+
+data.a  <- data.a %>%  
+  na.omit()
+
+data.a2 <- data.a %>% 
+  separate_rows(Auth_keywords, sep = ";")  #seperating based on ;
+remove(data.a)
+
+data.a2$Auth_keywords <- gsub(" ", "", data.a2$Auth_keywords) #removing space
+data.a2 <- as.data.frame(lapply(data.a2, tolower)) #all lower case
+data.a2$Year <- as.numeric(as.character(data.a2$Year))
+
+#1996 - 2014 -------
+data.a2.96_14 <- data.a2 %>% 
+  filter(Year >= 1996 & Year <= 2015) %>% 
+  select(2) #1807 key words
+
+a2.96_14 <- Corpus(VectorSource(data.a2.96_14))
+
+a2.96_14.matrix <- TermDocumentMatrix(a2.96_14)
+a2.96_14.matrix1 <- as.matrix(a2.96_14.matrix)
+a2.96_14.v <- sort(rowSums(a2.96_14.matrix1),decreasing=TRUE)
+a2.96_14.d <- data.frame(word = names(a2.96_14.v),freq=a2.96_14.v)
+
+remove(data.a2.96_14, a2.96_14, a2.96_14.matrix, a2.96_14.matrix1, a2.96_14.v)
+
+#2015 - 2020 -------
+data.a2.15_20 <- data.a2 %>% 
+  filter(Year >= 2015 & Year <= 2020) %>% 
+  select(2) #2764 key words
+
+a2.15_20 <- Corpus(VectorSource(data.a2.15_20))
+
+a2.15_20.matrix <- TermDocumentMatrix(a2.15_20)
+a2.15_20.matrix1 <- as.matrix(a2.15_20.matrix)
+a2.15_20.v <- sort(rowSums(a2.15_20.matrix1),decreasing=TRUE)
+a2.15_20.d <- data.frame(word = names(a2.15_20.v),freq=a2.15_20.v)
+
+remove(data.a2.15_20, a2.15_20, a2.15_20.matrix, a2.15_20.matrix1, a2.15_20.v)
+
+# write.csv(a2.96_14.d, file="output/author-96_14.csv")
+# write.csv(a2.15_20.d, file="output/author-15_20.csv")
+
+#Index Keyword ----------
+data.i <- data0 %>% 
+  select(4, 19)
+
+sum(is.na(data.i$Index_keywords)) #388 rows are NA
+
+data.i  <- data.i %>%  
+  na.omit()
+
+data.i2 <- data.i %>% 
+  separate_rows(Index_keywords, sep = ";")  #seperating based on ;
+remove(data.i)
+
+data.i2$Index_keywords <- gsub(" ", "", data.i2$Index_keywords) #removing space
+data.i2 <- as.data.frame(lapply(data.i2, tolower)) #all lower case
+data.i2$Year <- as.numeric(as.character(data.i2$Year))
+
+#1996 - 2014 -------
+data.i2.96_14 <- data.i2 %>% 
+  filter(Year >= 1996 & Year <= 2015) %>% 
+  select(2) #4287 key words
+
+i2.96_14 <- Corpus(VectorSource(data.i2.96_14))
+
+i2.96_14.matrix <- TermDocumentMatrix(i2.96_14)
+i2.96_14.matrix1 <- as.matrix(i2.96_14.matrix)
+i2.96_14.v <- sort(rowSums(i2.96_14.matrix1),decreasing=TRUE)
+i2.96_14.d <- data.frame(word = names(i2.96_14.v),freq=i2.96_14.v)
+
+remove(data.i2.96_14, i2.96_14, i2.96_14.matrix, i2.96_14.matrix1, i2.96_14.v)
+
+#2015 - 2020 -------
+data.i2.15_20 <- data.i2 %>% 
+  filter(Year >= 2015 & Year <= 2020) %>% 
+  select(2) #5497 key words
+
+i2.15_20 <- Corpus(VectorSource(data.i2.15_20))
+
+i2.15_20.matrix <- TermDocumentMatrix(i2.15_20)
+i2.15_20.matrix1 <- as.matrix(i2.15_20.matrix)
+i2.15_20.v <- sort(rowSums(i2.15_20.matrix1),decreasing=TRUE)
+i2.15_20.d <- data.frame(word = names(i2.15_20.v),freq=i2.15_20.v)
+
+remove(data.i2.15_20, i2.15_20, i2.15_20.matrix, i2.15_20.matrix1, i2.15_20.v)
+
+# write.csv(i2.96_14.d, file="output/index-96_14.csv")
+# write.csv(i2.15_20.d, file="output/index-15_20.csv")
