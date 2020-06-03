3f0a602689f0d34b21ee17ae35d51f2dbb7047f0
KaranSShakya <karan.fingo@gmail.com>
2020-06-03 09:20:17 -0700

    line graph final

--- code/syn_4-linegraph.R
+++ code/syn_4-linegraph.R
@@ -1,3 +1,4 @@
+#Library------
 library(tidyverse)
 library(dplyr)
 library(readxl)
@@ -8,26 +9,103 @@ key <- read_csv("data/hkh-keyword.csv") %>%
   select(-1) %>% 
   mutate(id=seq(1:13115)) %>% 
   select(3,1,2)
+names(key)[3] <- "Keyword"
 
 #
 #Group division-----
-found <- c("cropdiversity", "livestock", "geneticdiversity", "landraces", 
-  "paddylandraces", "ricelandraces", "traditionalcrops", "cropproduction",
-  "cropimprovement", "germplasm")
+# found <- c("cropdiversity", "livestock", "geneticdiversity", "landraces", 
+#   "paddylandraces", "ricelandraces", "traditionalcrops", "cropproduction",
+#   "cropimprovement", "germplasm")
+# 
+# func <- c("ecosystemservice", "ecosystemservices", "traditionalknowledge",
+#           "integratedfarming", "integratedapproach", "integratedfarmingsystems",
+#           "integratedapproach", "sustainableintensification", "conservation", 
+#           "conservationagriculture", "ecologicalagriculture", "	integratedpestmanagement",
+#           "soilnutrient", "watermanagement")
+# 
+# appli <- c("foodsecurity", "agribusiness", "foodsupply", "agritourism",
+#            "livelihood", "livelihoods", "farmincome", "nutrition")
+# 
+# engage <- c("sustainability", "sustainableagriculture", "adaptation",
+#             "resilience", "ecosystemresilience")
+# 
+# drive <- c("climatechange", "landusechange", "pesticide", "pesticides")
 
-func <- c("ecosystemservice", "ecosystemservices", "traditionalknowledge",
-          "integratedfarming", "integratedapproach", "integratedfarmingsystems",
-          "integratedapproach", "sustainableintensification", "conservation", 
-          "conservationagriculture", "ecologicalagriculture", "	integratedpestmanagement",
-          "soilnutrient", "watermanagement")
+#Group Import-------
+found <- read_excel("data/keyword-groups.xlsx", 
+                    sheet = "foundation")
+func <- read_excel("data/keyword-groups.xlsx", 
+                    sheet = "functional")
+appli <- read_excel("data/keyword-groups.xlsx", 
+                    sheet = "application")
+engage <- read_excel("data/keyword-groups.xlsx", 
+                    sheet = "engagement")
+drivers <- read_excel("data/keyword-groups.xlsx", 
+                    sheet = "drivers")
 
-appli <- c("foodsecurity", "agribusiness", "foodsupply", "agritourism",
-           "livelihood", "livelihoods", "farmincome", "nutrition")
+#Merge
+key.f <- merge(key, found, by="Keyword")
+key.fu <- merge(key, func, by="Keyword")
+key.a <- merge(key, appli, by="Keyword")
+key.e <- merge(key, engage, by="Keyword")
+key.d <- merge(key, drivers, by="Keyword")
 
-engage <- c("sustainability", "sustainableagriculture", "adaptation",
-            "resilience", "ecosystemresilience")
+remove(found, func, appli, engage, drivers)
 
-drive <- c("climatechange", "landusechange", "pesticide", "pesticides")
 
-#Division-------
 
+
+
+#Function test--------
+key.f1 <- key.f %>% 
+  select(3,1) %>% 
+  group_by(Year) %>% 
+  tally()
+names(key.f1)[2] <- "foundation"
+
+key_count <- function(x){
+  x1 <- x %>% 
+    select(3,1) %>% 
+    group_by(Year) %>% 
+    tally()
+}
+
+key.fu1 <- key_count(key.fu)
+names(key.fu1)[2] <- "functional"
+key.a1 <- key_count(key.a)
+names(key.a1)[2] <- "application" 
+key.d1 <- key_count(key.d)
+names(key.d1)[2] <- "drivers"
+key.e1 <- key_count(key.e)
+names(key.e1)[2] <- "engagement"
+
+remove(key.a, key.d, key.e, key.f, key.fu)
+
+
+
+#Merge all--------
+all <- merge(key.f1, key.fu1, by="Year", all=T)
+all <- merge(all, key.a1, by="Year", all=T)
+all <- merge(all, key.e1, by="Year", all=T)
+all <- merge(all, key.d1, by="Year", all=T)
+
+all[is.na(all)] <- "0"
+
+all.l <- all %>% 
+  gather(key="Attribute", value="Freq", 2:6)
+
+all.l$Freq <- as.numeric(as.character(all.l$Freq))
+
+all.final <- all.l %>% 
+  group_by(Year, Attribute) %>% 
+  summarise(Sum = sum(Freq)) %>% 
+  ungroup()
+
+
+raw <- ggplot(all.final, aes(x=Year, y=Sum))+
+  geom_line(aes(color=Attribute))+
+  labs(y="Count of Keywords")
+
+geom_area <- ggplot(all.final, aes(x=Year, y=Sum))+
+  geom_area(aes(color=Attribute))+
+  labs(y="Count of Keywords")
