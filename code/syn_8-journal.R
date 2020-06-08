7c50825f57e06188570eff6c6901d06b5fcc6f42
KaranSShakya <karan.fingo@gmail.com>
2020-06-08 10:26:45 -0700

    edits (2/2)

--- code/syn_8-journal.R
+++ code/syn_8-journal.R
@@ -1,1 +1,24 @@
 library(tidyverse)
+library(readxl)
+
+data0 <- read_excel("data/synthesis_HKH-final.xlsx")
+
+data1 <- data0 %>% 
+  select(5)
+names(data1)[1] <- "Journal"
+
+data.sum <- data1 %>% 
+  group_by(Journal) %>% 
+  tally() %>% 
+  ungroup() %>% 
+  na.omit() %>% 
+  arrange(desc(n)) %>% 
+  head(17)
+names(data.sum)[2] <- "Count"
+
+remove(data0, data1)
+
+
+
+
+
