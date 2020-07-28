3f25cb8f9ed43dd9f25625f3100b78453bf8822c
KaranSShakya <karan.fingo@gmail.com>
2020-07-28 16:23:15 -0700

    bioplot edit + scree plot

--- code/extra_2-PCA_agrobiodiversity.R
+++ code/extra_2-PCA_agrobiodiversity.R
@@ -5,7 +5,7 @@ library(factoextra)
 pca0 <- read_excel("data/pca_agrobiodiversity.xlsx", 
                    sheet = "Edited")
 pca1 <- pca0 %>%
-  select(-1) %>% 
+  select(-1,-4) %>% 
   remove_rownames() %>% 
   column_to_rownames(var="Acronym")
 
@@ -14,6 +14,23 @@ res.pca <- prcomp(pca1, scale=T)
 fviz_pca_ind(res.pca, repel = T)
 fviz_pca_ind(res.pca, repel = T, label=F)
 
-biplot <- fviz_pca_biplot(res.pca, repel = T)
+biplot2 <- fviz_pca_biplot(res.pca, repel = T) 
 
-ggsave(filename = "biplot_agrobiodiversity_draft1.png", plot=biplot, device = "png", dpi=300)
+#ggsave(filename = "biplot_agrobiodiversity_draft2.png", plot=biplot2, device = "png", dpi=300)
+
+#plot(res.pca, type="lines")
+
+#screeplot(res.pca, type="lines")
+
+scree <- fviz_eig(res.pca, choice="eigenvalue",
+         addlabels = TRUE,
+         geom="line")
+
+ggsave(filename = "scree_draft1.png", plot=scree, device = "png", dpi=300)
+
+
+
+
+
+
+
