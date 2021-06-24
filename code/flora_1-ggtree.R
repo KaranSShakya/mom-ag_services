4d7f56e0e86828e14d7b7a700a4cb211cc36e959
KaranSShakya <karan.fingo@gmail.com>
2021-06-24 17:31:11 -0400

    try 2 - phylogenetic

---
+++ code/flora_1-ggtree.R
@@ -0,0 +1,70 @@
+# Library ----
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(ggtree)
+library(TreeSummarizedExperiment)
+
+# library(ape)
+# library(data.tree)
+# library(treemap)
+
+# Data ----
+data.0 <- read_excel("data/flora-raw.xlsx", 
+                        sheet = "final revised")
+data.1 <- data.0 %>% 
+  select(1,2,3) 
+
+data.2 <- data.0 %>% 
+  select(1,2,3,4) 
+data.2dub <- data.2[!duplicated(data.2$family), ]
+
+# ggtree:Order ----
+data.tree <- toTree(data = data.1)
+
+# ggtree(data.tree, layout = "circular")+
+#   geom_text2(aes(label=label), color="red", vjust=1,
+#              position = position_dodge(width=0.9),
+#              check_overlap = TRUE)+
+#   geom_nodepoint()
+
+fig1 <- ggtree(data.tree, layout = "circular")+
+  geom_nodepoint()+
+  geom_tiplab(size=2, aes(angle=angle))
+
+# ggtree:Family ----
+data.tree2 <- toTree(data = data.2dub)
+
+fig2 <- ggtree(data.tree2, layout = "circular")+
+  geom_tiplab(size=1, aes(angle=angle))
+
+# Examples ----
+## Example 1 ----
+    # data(GNI2014)
+    # GNI2014$pathString <- paste("world", 
+    # 
+    #                             GNI2014$continent, 
+    #                             GNI2014$country, 
+    #                             sep = "/")
+    # 
+    # population <- as.Node(GNI2014)
+    # print(population, "iso3", "population", "GNI", limit = 20)
+
+## Example 2 ----
+    # taxTab <- data.frame(R1 = rep("A", 5),
+    #                      R2 = c("B1", rep("B2", 4)),
+    #                      R3 = paste0("C", 1:5))
+    # 
+    # tree <- toTree(data = taxTab)
+    # 
+    # ggtree(tree) + 
+    #   geom_text2(aes(label = label), color = "red", vjust = 1) + 
+    #   geom_nodepoint()
+
+
+
+
+
+
+
+
